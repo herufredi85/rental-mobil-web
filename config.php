eac604fa0057e8b2d5b5d86d5c18ce086de66587
Heru <herufredi85@gmail.com>
2023-12-12 14:16:02 +0700

    update last

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 if(!defined('BASEPATH')) echo "Tidak bisa langsung mengakses halaman ini!";
 
 define('DEFAULT_CONTROLLER', 'C_Auth');
-define('BASE_URL', 'http://localhost/rental-mobil/');
+define('BASE_URL', 'https://7e10-2001-448a-8060-826-94c9-b61f-d7e4-7ea3.ngrok-free.app/rental-mobil/');
 define('APP_NAME', 'Aplikasi Rental Mobil');
 
 define('DB_HOST', 'localhost');
